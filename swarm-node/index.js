1a538080c7ac8294134f4307308f034f459ebab9
NuwanJ <nuwanjaliyagoda@gmail.com>
2020-10-17 16:50:10 +0530

    updates

--- swarm-node/index.js
+++ swarm-node/index.js
@@ -2,12 +2,7 @@ const { setup } = require('./services/protocols');
 const { move, moveSpecific, stop, reset } = require('./services/motor/');
 const logger = require('./logger/winston');
 
-var coordinates = {
-    head: 0,
-    x: 0,
-    y: 0
-}
-
+var coordinates = {head: 0,x: 0,y: 0}
 var robotId = undefined;
 
 logger.info('Initiated ROBOT(%s) instance with coordinates: %s', robotId, coordinates);
