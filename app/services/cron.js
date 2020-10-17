dd66dfdca18ca030260de0956ce1058f54615b43
NuwanJ <nuwanjaliyagoda@gmail.com>
2020-10-17 18:51:39 +0530

    MQTT structure

--- app/services/cron.js
+++ app/services/cron.js
@@ -1,11 +1,5 @@
 'use strict'
 
-const sensorUpdate = require("../cron/sensor.update.cron.js");
-var mqtt;
-
 exports.begin = (mqtt) => {
-   this.mqtt = mqtt;
-
-   sensorUpdate.update(mqtt, 1009, 5); 
 
 }
