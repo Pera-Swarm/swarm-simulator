14a722e81e967a6eeb83233b15560fa8bebe641d
luk3Sky <ganindudananja@gmail.com>
2020-10-25 18:29:39 +0530

    swarm-node update with modules

--- swarm-node/config/mqtt.js
+++ swarm-node/config/mqtt.js
@@ -1,9 +1,16 @@
 module.exports = {
     HOST: process.env.MQTT_HOST,
+    port: 1883,
     options: {
         port: 1883,
-        clientId: process.env.MQTT_CLIENT
-        // username: process.env.MQTT_USER,
-        // password: process.env.MQTT_PASS,
+        clientId: process.env.MQTT_CLIENT,
+        clientId: 'client' + Math.random().toString(16).substr(2, 8),
+        username: process.env.MQTT_USER || '',
+        password: process.env.MQTT_PASS || ''
+    },
+    mqttOptions: {
+        qos: 2,
+        rap: true,
+        rh: true
     }
 };
--- swarm-node/index.js
+++ swarm-node/index.js
@@ -1,13 +1,38 @@
-const { setup } = require('./services/protocols');
-const { move, moveSpecific, stop, reset } = require('./services/motor/');
 const logger = require('./logger/winston');
-const Robot = require('./robot');
 
-var robot = new Robot();
+// External modules
+const mqttClient = require('mqtt');
+const mqttConfig = require('./config/mqtt');
+const { mqttOptions } = require('./config/mqtt');
+const mqtt = mqttClient.connect(mqttConfig.HOST, mqttConfig.options);
+const { MQTTRouter, publishToTopic, wrapper } = require('../app/modules/mqtt-handler/');
+const { Robot } = require('../app/modules/robot/');
+const { defineBaseMode } = require('../app/modules/modes/');
 
-setInterval(() => {
-    if (robot.getId() === undefined) {
+// Internal modules
+// const { move, moveSpecific, stop, reset } = require('./robot/controllers/index');
+const { setup } = require('./robot/services/protocols');
+const { robotRoutes } = require('./robot/controllers/mqtt/');
+
+var robot;
+var mqttRouter;
+
+initiate = () => {
+    robot = new Robot(1);
+    mqttRouter = new MQTTRouter(
+        mqtt,
+        //myRoutes,
+        wrapper([...robotRoutes], robot),
+        mqttOptions
+        // setup
+    );
+    mqttRouter.start();
+};
+
+loop = () => {
+    if (robot.id === undefined) {
         logger.log('debug', 'main: ROBOT ID Registration initial');
+        console.log(robot);
         setup.registerId((success, id) => {
             if (success) {
                 robot.setId(id);
@@ -18,7 +43,12 @@ setInterval(() => {
             }
         });
     } else {
+        // console.log(robot);
         // coordinates = move(coordinates);
         // logger.info('main: Executing ROBOT(%s) instance with coordinates: %s', robotId, coordinates);
     }
-}, 100);
+};
+
+defineBaseMode(initiate, loop, 100, 'swarm-node-instance-mode');
+
+// var uuid =
