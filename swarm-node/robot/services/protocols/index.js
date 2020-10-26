a96022e2faab8826458ff48f5522f22de3d3ba27
luk3Sky <ganindudananja@gmail.com>
2020-10-26 13:21:00 +0530

    swarm-node heartbeat and localization info update

--- swarm-node/robot/services/protocols/index.js
+++ swarm-node/robot/services/protocols/index.js
@@ -1,7 +1,10 @@
 const { generateId, heartbeat } = require('./id');
+const { localizationInfoUpdate } = require('./localization');
 const setup = require('./setup');
 
 module.exports = {
     generateId,
+    heartbeat,
+    localizationInfoUpdate,
     setup
 };
---
+++ swarm-node/robot/services/protocols/localization.js
@@ -0,0 +1,33 @@
+const logger = require('../../../logger/winston');
+
+const { MqttClient } = require('mqtt');
+const { publishToTopic } = require('.././../../../app/modules/mqtt-handler');
+const { Robot } = require('../../../../app/modules/robot');
+
+/**
+ * method for sending the localization info
+ * @param {Robot} robot 
+ * @param {MqttClient} mqtt 
+ * @param {object} mqttOptions 
+ */
+const localizationInfoUpdate = (robot, mqtt, mqttOptions) => {
+    const message = {
+        id: robot.id,
+        heading: robot.coordinate.heading,
+        x: robot.coordinate.x,
+        y: robot.coordinate.y
+    };
+
+    // this will send a MQTT request to the topic 'v1/localization/info'
+    publishToTopic(mqtt, 'v1/localization/info', JSON.stringify(message), mqttOptions, () => {
+        logger.log(
+            'debug',
+            'robot.services.protocols.localization.localizationInfoUpdate: Heartbeat (%s) success',
+            message
+        );
+    });
+}
+
+module.exports = {
+    localizationInfoUpdate
+}
