aaa62b717057dd9b3579ca72573cca6f920d5c17
Ganindu Dhananja <ganindudananja@gmail.com>
2020-10-27 00:20:22 +0530

    Update localization.js

--- app/swarm/controllers/mqtt/localization.js
+++ app/swarm/controllers/mqtt/localization.js
@@ -5,22 +5,24 @@ const routes = [
     {
         topic: 'v1/localization/info',
         allowRetained: true,
+        subscribe: true,
         handler: (msg, swarm) => {
             //console.log('MQTT_Localization:Info_Handler', msg);
 
             // TODO: write this function
             swarm.robots.locationUpdate(msg);
-        },
-        subscribe: true
+            // TODO: swarm.updateLocalization(msg.id, 'distance');
+            swarm.publish('v1/robot/live', 'sample msg loc');
+        }
     },
     {
         topic: 'v1/robot/create',
         allowRetained: true,
+        subscribe: true,
         handler: (msg, swarm) => {
             //console.log('Creating > id:',msg.id,'x:',msg.x,'y:',msg.y);
             swarm.robots.addRobot(msg.id, msg.x, msg.y, msg.heading);
-        },
-        subscribe: true
+        }
     }
 ];
 
