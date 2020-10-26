221c88b39e563a0fcf155940f6abe9dd3278fa31
luk3Sky <ganindudananja@gmail.com>
2020-10-26 15:07:01 +0530

    swamr-node circular pattern function update

--- swarm-node/robot/controllers/patterns/circular.js
+++ swarm-node/robot/controllers/patterns/circular.js
@@ -10,7 +10,7 @@ function move(x, y, heading) {
 
 const circular = (robot, callback) => {
     logger.log('debug', 'controllers.patterns.circular: ', robot.getCoordinates());
-    heading = 90 - a;
+    heading = 180 - a;
     x = R * Math.cos((a * Math.PI) / 180);
     y = R * Math.sin((a * Math.PI) / 180);
     a = a + delta;
