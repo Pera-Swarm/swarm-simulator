04ce89ff8ff964e57d9e063a8e17567e0c832b5c
NuwanJ <nuwanjaliyagoda@gmail.com>
2020-10-26 18:43:51 +0530

    Updates

--- app/swarm/swarm.js
+++ app/swarm/swarm.js
@@ -17,7 +17,7 @@ const { SimpleLocalizationSystem } = require('../modules/localization');
 // const cron = require('../services/cron.js');
 
 // Controllers
-const { localizationRoutes, sensorRoutes } = require('./controllers/mqtt/');
+const { localizationRoutes, sensorRoutes, controlRoutes } = require('./controllers/mqtt/');
 const { initRobots } = require('./robots/robots');
 
 const SAMPLE_ROUTES = [
@@ -54,17 +54,6 @@ class Swarm {
      * @param {function} setup a fuction to run when the swarm object created
      */
     constructor(setup) {
-        const myRoutes = [
-            {
-                topic: 'v1/gui/create',
-                allowRetained: true,
-                handler: (topic, msg) => {
-                    //console.log('Creating > id:',msg.id,'x:',msg.x,'y:',msg.y);
-                    this.robots.addRobot(msg.id);
-                }
-            }
-        ];
-
         //console.log(wrapper(myRoutes, this.robots));
 
         this.loc_system = new SimpleLocalizationSystem();
@@ -72,7 +61,7 @@ class Swarm {
         this.mqttRouter = new MQTTRouter(
             mqtt,
             //myRoutes,
-            wrapper([...localizationRoutes, ...sensorRoutes], this),
+            wrapper([...localizationRoutes, ...sensorRoutes, ...controlRoutes], this),
             mqttOptions,
             setup
         );
@@ -105,8 +94,8 @@ class Swarm {
      * @param {string} message mqtt message object
      */
     publish = (topic, message) => {
-        publishToTopic(mqtt, topic, message, mqttOptions, () => {
-            console.log(`publish message > ${message} to topic ${topic}`);
+        publishToTopic(mqtt, topic, message.toString(), mqttOptions, () => {
+            console.log(`MQTT_Publish > ${message} to topic ${topic}`);
         });
     };
 }
