d27a61dd968490709ccf10108a79617cabc4e239
NuwanJ <nuwanjaliyagoda@gmail.com>
2020-10-26 18:43:24 +0530

    New MQTT route definitions

---
+++ app/swarm/controllers/mqtt/route.control.js
@@ -0,0 +1,33 @@
+// control routes and handlers
+// Note: 'swarm' argument will be added via wrapper
+
+const routes = [
+    {
+        topic: 'v1/robot/live',
+        allowRetained: false,
+        handler: (msg, swarm) => {
+            //console.log('UpdatingHeartbeat > id:',msg.id,'x:',msg.x,'y:',msg.y);
+            var robot = swarm.robots.findRobotById(msg.id);
+
+            if(robot != undefined){
+                const heartbeat = robot.updateHeartbeat();
+                console.log('Heatbeat of the robot', msg.id, 'is updated to', heartbeat);
+            }else{
+                // No robot found.
+            }
+        },
+        subscribe: true,
+    },
+    {
+        topic: 'v1/robot/create',
+        allowRetained: true,
+        handler: (msg, swarm) => {
+            //console.log('Creating > id:',msg.id,'x:',msg.x,'y:',msg.y);
+            swarm.robots.addRobot(msg.id, msg.x, msg.y, msg.heading);
+        },
+        subscribe: true,
+    },
+
+];
+
+module.exports = routes;
---
+++ app/swarm/controllers/mqtt/route.sensors.js
@@ -0,0 +1,27 @@
+// sensor routes and handlers
+// Note: 'swarm' argument will be added via wrapper
+
+const routes = [
+    {
+        topic: 'v1/sensor/distance',
+        allowRetained: true,
+        handler: (msg, swarm) => {
+            console.log('MQTT_Sensor:Distance_Handler: ', msg);
+
+            var robot = swarm.robots.findRobotById(msg.id);
+
+            if(robot != undefined){
+                var returnValue = robot.sensors.distance.syncReading(msg.distance);
+                swarm.publish('v1/sensor/distance/' + robot.id, returnValue);
+                
+            }else{
+                // No robot found. Just echo the message, because this is a blocking call for the robot
+                // TODO: register the robot into system
+                swarm.publish('v1/sensor/distance/' + msg.id, msg.distance);
+            }
+        },
+        subscribe: true,
+    }
+];
+
+module.exports = routes;
