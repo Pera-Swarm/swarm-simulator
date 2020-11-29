0820df7288e36b80715b01464801204f356482f3
NuwanJ <nuwanjaliyagoda@gmail.com>
2020-11-29 23:42:24 +0530

    swarm-node algo updated with coordinate system

--- swarm-node/robot/controllers/patterns/linear.js
+++ swarm-node/robot/controllers/patterns/linear.js
@@ -3,14 +3,14 @@ const logger = require('../../../logger/winston');
 function move(robot, d) {
     var { x, y, heading } = robot.getCoordinates();
 
-    x = x + d * Math.cos((-1*heading * Math.PI) / 180);
-    y = y + d * Math.sin((-1*heading * Math.PI) / 180);
+    x = x + d * Math.cos((heading * Math.PI) / 180);
+    y = y + d * Math.sin((heading * Math.PI) / 180);
 
     x = Math.round(x * 100) / 100;
     y = Math.round(y * 100) / 100;
-    
+
     if (Math.abs(x) > 140 || Math.abs(y) > 140) {
-        const turn = 180 + (Math.random() * 90 - 45);
+        const turn = 180 + ((Math.random() * 120)- 60);
         heading = Math.round(normalizedAngle(heading + turn) * 10) / 10;
     }
     console.log(x, y, heading);
@@ -27,7 +27,7 @@ function normalizedAngle(a) {
 
 const linear = (robot, callback) => {
     //logger.log('debug', 'controllers.patterns.linear: ', robot.getCoordinates());
-    move(robot, 10);
+    move(robot, 30);
 
     callback();
 };
