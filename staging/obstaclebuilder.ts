e04ca700c2f73a2f39faeb767ca0998f644a6d1e
Ganindu <ganindudananja@gmail.com>
2021-01-03 19:40:29 +0530

    Create obstaclebuilder.ts

---
+++ staging/obstaclebuilder.ts
@@ -0,0 +1,70 @@
+import { AbstractCylinder, Cylinder } from './cylinder';
+import { AbstractWall, Wall } from './wall';
+import { AbstractObject } from './obstacle';
+
+interface AbstractObstacleBuilder {
+    createWall(
+        width: number,
+        height: number,
+        orientation: number,
+        originX: number,
+        originY: number,
+        depth: number,
+        debug: boolean
+    ): AbstractWall;
+    createCylinder(
+        radius: number,
+        height: number,
+        originX: number,
+        originY: number,
+        debug: boolean
+    ): AbstractCylinder;
+
+    changeMaterial(obstacle: AbstractObject, materialType: string): void;
+}
+
+class ObstacleBuilder implements AbstractObstacleBuilder {
+    protected static instance: ObstacleBuilder;
+    protected list: AbstractObject[];
+
+    constructor() {
+        this.list = [];
+    }
+
+    public static getInstance(): ObstacleBuilder {
+        if (!ObstacleBuilder.instance) {
+            ObstacleBuilder.instance = new ObstacleBuilder();
+        }
+        return ObstacleBuilder.instance;
+    }
+
+    createWall(
+        width: number,
+        height: number,
+        orientation: number,
+        originX: number,
+        originY: number,
+        depth: number,
+        debug: boolean
+    ): AbstractWall {
+        return new Wall(width, height, orientation, originX, originY, depth, debug);
+    }
+
+    createCylinder(
+        radius: number,
+        height: number,
+        originX: number,
+        originY: number,
+        debug: boolean
+    ): AbstractCylinder {
+        return new Cylinder(radius, height, originX, originY, debug);
+    }
+
+    changeMaterial = (obstacle: AbstractObject, materialType: string) => {
+        obstacle.setMaterial(materialType);
+    };
+}
+
+export const obstacleBuilder = () => {
+    return ObstacleBuilder.getInstance();
+};
