161e10da1e3b7fb8a6fde0e5677c3f81fb823f31
Ganindu <ganindudananja@gmail.com>
2021-01-03 18:47:43 +0530

    Update testbed

--- testbed/index.js
+++ testbed/index.js
@@ -1,6 +1,7 @@
 const { WallObstacle, CylinderObstacle } = require('../app/modules/obstacles/');
-const { Cylinder } = require('../staging/cylinder');
-const { Wall } = require('../staging/wall');
+const { obstacleBuilder } = require('../dist/obstaclebuilder');
+const { Cylinder } = require('../dist/cylinder');
+const { Wall } = require('../dist/wall');
 
 const x = 40;
 const y = 40;
@@ -13,9 +14,13 @@ const radius = 10;
 const originX = 15;
 const originY = 15;
 
+const builder = obstacleBuilder();
+console.log(builder);
+
 // id, radius,height, originX, originY, debug = false
 // const c = new CylinderObstacle(1, radius, height, originX, originY, true);
-const c = new Cylinder(radius, height, originX, originY, true);
+// const c = new Cylinder(radius, height, originX, originY, true);
+const c = builder.createCylinder(radius, height, originX, originY, true);
 
 const range = c.isInRange(heading, x, y, 5);
 const ans = c.getDistance(heading, x, y);
@@ -23,21 +28,11 @@ const ans = c.getDistance(heading, x, y);
 console.log(range, ans);
 // console.log(c.visualize());
 
-/**
- * 
-Cylinder obstacle created
-radius: 10 height: 20
-p: { x: 15, y: 15 }
-
-Calculated Angle: -135, deltaT: 16.427713126242697
-heading: -135 CCW: -113.57 CW: -156.43
-true 25.36
- */
-
 //const wall1 = new WallObstacle(1, 100,20, 0, -50, 50, true);
 //const ans = wall1.isInRange(0, 0, 0);
 // width, height, orientation, originX, originY, depth, debug=false
-const w = new Wall(100, 20, 0, -50, -50, depth, true);
+// const w = new Wall(100, 20, 0, -50, -50, depth, true);
+const w = builder.createWall(100, 20, 0, -50, -50, depth, true);
 const range2 = w.isInRange(heading, x, y, 5);
 const ans2 = w.getDistance(heading, x, y);
 
