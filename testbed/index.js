4b616e8a65623e4b6a5e2d2caa88be5c56ef6aea
NuwanJ <nuwanjaliyagoda@gmail.com>
2021-01-04 18:27:10 +0530

    wall obstacle distance function updates

--- testbed/index.js
+++ testbed/index.js
@@ -1,13 +1,26 @@
 const { WallObstacle, CylinderObstacle } = require('../app/modules/obstacles/');
 
-const width = 100;
+const wallWidth = 100;
+const wallX = -50;
+const wallY = 50;
+const wallOrietation = 45;
+
 const x = 0;
 const y = 0;
-const heading = 90;
+const heading = 85;
 
-//const wall1 = new WallObstacle(1, 100,20, 0, -50, 50, true);
-//const ans = wall1.isInRange(0, 0, 0);
+// id, width, height, orientation, originX, originY, debug = false
+const obs = new WallObstacle(1, wallWidth, 20, wallOrietation, wallX,wallY, true);
+
+const range = obs.isInRange(heading, x, y, 5);
+const ans = obs.getDistance(heading, x, y);
+
+console.log(range, ans);
+
+
 /*
+// For cylinder obstacle
+
 const radius = 10;
 const height = 20;
 const originX = 15;
@@ -15,22 +28,10 @@ const originY = 15;
 
 id, radius,height, originX, originY, debug = false
 const c = new CylinderObstacle(1, radius, height, originX, originY, true);
-*/
-
-// id, width, height, orientation, originX, originY, debug = false
-const obs = new WallObstacle(1, 100, 20, 0, -50, 50, true);
 
 const range = obs.isInRange(heading, x, y, 5);
 const ans = obs.getDistance(heading, x, y);
 
 console.log(range, ans);
 
-/*const line = obs._getLine(3,2,1.24904577);
-const point = obs._getIntersectionPoint(1,2,1,2,3,5);
-const dd= obs._getDistancebetween2points(3,2,9,7);
-
-console.log('line: '+line);
-console.log('point: '+point);
-console.log('distance: '+dd);*/
-
-obs._getDistanceAlongHeading(0,0,30,-2,-2,360);
+*/
