bb496799c3246cc699d16c9642afcc9c98d0e48a
Ganindu <ganindudananja@gmail.com>
2021-01-16 21:21:41 +0530

    Swarm server routes

---
+++ app/swarm/mqtt/route.obstacles.js
@@ -0,0 +1,20 @@
+// obstacle routes and handlers
+// Note: 'swarm' argument will be added via wrapper
+
+const routes = [
+    {
+        topic: 'obstacles/?',
+        type: 'String',
+        allowRetained: true,
+        subscribe: true,
+        publish: false,
+        handler: (msg, swarm) => {
+            // Heartbeat signal from the robots to server
+            console.log('MQTT.Obstacles: obstacles/?', msg);
+            // console.log(swarm.obstacleController.visualizeObstacles());
+            // swarm.mqttPublish('obstacles', swarm.obstacleController.visualizeObstacles());
+        }
+    }
+];
+
+module.exports = routes;
