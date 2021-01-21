ecbf03a1de4ad57136c4433c34da5fb9a3d48e32
Ganindu <ganindudananja@gmail.com>
2021-01-21 20:47:10 +0530

    Env JSON config support

--- staging/pera-swarm/modules/environment/environment.ts
+++ staging/pera-swarm/modules/environment/environment.ts
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import { AbstractObject, AbstractObstacleController } from '../obstacles/';
 
 export type ArenaType = {
@@ -5,69 +6,130 @@ export type ArenaType = {
     xMax: number;
     yMin: number;
     yMax: number;
-    units?: "virtual";
-}
+    units?: 'virtual';
+};
 
 export type EnvironmentConfig = {
     arena: any;
     obstacles: AbstractObject[];
     channel: string;
-}
+};
+
+const defaultConfig = {
+    arena: {
+        xMin: 150,
+        xMax: 150,
+        yMin: 150,
+        yMax: 150,
+        units: 'virtual'
+    },
+    obstacles: [],
+    channel: 'virtual'
+};
 
 /**
  * @abstract
  * @class AbstractEnvironment
  */
 export abstract class AbstractEnvironment {
-    _config: EnvironmentConfig;
+    _config: EnvironmentConfig | undefined;
     _obstacleController: AbstractObstacleController;
     _updated: number;
 
-    constructor(config: EnvironmentConfig, obstacleController: AbstractObstacleController) {
-        this._config = config;
+    constructor(
+        obstacleController: AbstractObstacleController,
+        config: EnvironmentConfig = defaultConfig
+    ) {
         this._obstacleController = obstacleController;
-        this._updated=Date.now();
+        this._config = config;
+        this._updated = Date.now();
     }
 
     /**
      * config
      */
-    get config(){
+    get config() {
         return this._config;
     }
 
     /**
      * obstacle controller
      */
-    get obstacleController(){
+    get obstacleController() {
         return this._obstacleController;
     }
 
+    abstract readConfig: Function;
     abstract updateConfig: Function;
+    abstract createObstacles: Function;
 }
 
 export class Environment extends AbstractEnvironment {
-    constructor(config: EnvironmentConfig, obstacleController: AbstractObstacleController) {
-        super(config, obstacleController);
+    constructor(obstacleController: AbstractObstacleController, file: string) {
+        super(obstacleController);
+        this._config = this.readConfig(file);
+    }
+
+    /**
+     * method for reading environment config
+     * @param {string} file environment config file path string
+     */
+    readConfig = (file: string): EnvironmentConfig | undefined => {
+        // Read config.json file
+        var config: EnvironmentConfig;
+        try {
+            const jsonString = fs.readFileSync(file);
+            // Converting to JSON
+            config = JSON.parse(jsonString.toString());
+            console.log('Config_Found!');
+            // Validating configuration
+            if (validateEnvConfig(config) === true) {
+                console.log('Valid config:', config);
+                return config;
+            } else {
+                throw new Error('Invalid config');
+            }
+        } catch (err) {
+            // Check for errors
+            console.error('Config_Read', err);
+            return undefined;
+        }
     };
+
     /**
      * method for updating environment config
      * @param {EnvironmentConfig} config environment config object
      */
-    updateConfig =(config: EnvironmentConfig) => {
-        if(validateEnvConfig(config) === true){
+    updateConfig = (config: EnvironmentConfig) => {
+        if (validateEnvConfig(config) === true) {
             this._config = config;
             this._updated = Date.now();
+        }
+    };
+
+    /**
+     * method for creating obstacles in the environment according to the config
+     */
+    createObstacles = () => {
+        if (validateEnvConfig(this.config) === true) {
+            // TODO: create obstacles in the env
+            console.log('create obstacles', this.config);
         }
     };
 }
 
 /**
  * method for validating a config object.
- * @param {EnvironmentConfig} config
+ * @param {EnvironmentConfig|undefined} config
  * @returns {boolean|-1} true if valid or -1 if not.
  */
-export const validateEnvConfig = (config: EnvironmentConfig): boolean | -1 => {
+export const validateEnvConfig = (
+    config: EnvironmentConfig | undefined
+): boolean | -1 => {
+    if (config === undefined) {
+        console.error('Invalid config!');
+        return -1;
+    }
     var validity: boolean | -1;
     var i: number;
     validity = -1;
@@ -85,4 +147,4 @@ export const validateEnvConfig = (config: EnvironmentConfig): boolean | -1 => {
         validity = true;
     }
     return validity;
-}
+};
