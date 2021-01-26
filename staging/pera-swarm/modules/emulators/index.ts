fd3b9c0932c48e60ec4a4e2d035883d263bf9bdf
Ganindu <ganindudananja@gmail.com>
2021-01-26 17:03:48 +0530

    VirtualEmulatorInterface & AbstractVirtualEmulator

---
+++ staging/pera-swarm/modules/emulators/index.ts
@@ -0,0 +1,27 @@
+export interface VirtualEmulatorInterface {
+    defaultSubscriptions: Function;
+}
+
+export abstract class AbstractVirtualEmulator implements VirtualEmulatorInterface {
+    protected _publish: Function;
+    protected _publishTopic: string;
+
+    constructor(publish: Function, publishTopic: string = '') {
+        this._publish = publish;
+        this._publishTopic = publishTopic;
+    }
+
+    abstract defaultSubscriptions: Function;
+
+    /**
+     * method for publishing the sensor readings
+     * @param {number} value message value
+     * @param {string} suffix an optional mqtt topic suffix
+     */
+    publish = (value: number, suffix: string = '') => {
+        this._publish(`${this._publishTopic}/${suffix}`, String(value));
+    };
+}
+
+export * from './sensor-emulators/distance';
+export * from './agent-emulators/neopixel';
