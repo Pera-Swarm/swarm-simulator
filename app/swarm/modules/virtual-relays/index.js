1779c9a1cbe17c2609e0e435435ee0d3e968ff12
Ganindu <ganindudananja@gmail.com>
2021-01-26 17:28:41 +0530

    Code removal

--- app/swarm/modules/virtual-relays/index.js
+++
@@ -1,9 +0,0 @@
-const { DistanceRelayModule } = require('./distance');
-const { LocalizationRelayModule } = require('./localization');
-const { NeoPixelRelayModule } = require('./neopixel');
-
-module.exports = {
-    DistanceRelayModule,
-    LocalizationRelayModule,
-    NeoPixelRelayModule
-};
