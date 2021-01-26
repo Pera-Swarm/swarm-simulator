09a0678c1f1ebd3cfe9ba09f78721e5d5f3f80dc
Ganindu <ganindudananja@gmail.com>
2021-01-26 16:30:23 +0530

    Swarm and robot states

---
+++ staging/pera-swarm/modules/robot/robot-states.ts
@@ -0,0 +1,17 @@
+export const ROBOT_STATES: string[] = [
+    // Basic States
+    'IDLE',
+    'UPDATING',
+    'WAITING',
+    // Agregation States
+    'SEARCHING',
+    'INCLUSTER',
+    'AGGREGATE',
+    // Pattern Formation
+    'ACK_PATTERN',
+    'INITIALIZING_PATTERN',
+    'JOINED_PATTERN',
+    'SYNC_PATTERN_INFO',
+    'TIMEOUT',
+    'EXIT_PATTERN'
+];
